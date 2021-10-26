4f733cb1799dc98c1886db5295316dfa06e9e867
duckduckstab1 <84234115+duckduckstab1@users.noreply.github.com>
2021-10-26 22:07:14 +0100

    I had to use the batch version, the exe one doesn't work.

---
+++ cvmdownloader.bat
@@ -0,0 +1,16 @@
+@title off
+@echo off
+title CollabVM WebApp Downloader + server
+echo In alpha, please report bugs.
+echo Only for Windows PCs!
+echo I hope you got git.
+pause
+echo Sorry for wasting time, starting the process.
+git clone https://github.com/computernewb/collab-vm-server
+echo Done, now installing the web app.
+git clone https://github.com/computernewb/collab-vm-web-app
+echo Done, now to rumble the important steps.
+echo Wait, you gotta do it yourself. If your a newbie, type collab-vm-server 6004 collabvm in the cvm folder you downloaded. It's located in your user account folder.
+echo Finished downloading.
+echo It's now safe to exit.
+pause
